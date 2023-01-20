c52abe5b04f42ab907c9de8eed803ad116368e99
Drew White <d.white0002@gmail.com>
2023-01-19 16:43:35 -0800

    add stg premise

--- shared_dbt/models/staging/los_angeles/stg_la_premise.sql
+++ shared_dbt/models/staging/los_angeles/stg_la_premise.sql
@@ -1,18 +1,17 @@
-with source as (
- select * from {{ source('los_angeles', 'LA_2010_2019') }}
- union all
- select * from {{ source('los_angeles', 'LA_2020_PRESENT') }}
-),
+with stg_premise as (
+    select * from {{ ref('stg_los_angeles') }}
+), 
 
-staged as (
-
-    select 
+final as (
+    select distinct
       premis_cd,
       premis_desc,
       current_timestamp as created_at,
       current_timestamp as modified_at
+    
+    from stg_premise
+    where coalesce(premis_cd, premis_desc) is not null
 
-    from source
 )
 
-select * from staged
+select * from final
