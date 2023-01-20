375e67403746aac512b8bd51511398e51feb7d53
Drew White <d.white0002@gmail.com>
2023-01-20 09:44:42 -0800

    minor changes in sql syntax

--- shared_dbt/models/staging/los_angeles/stg_la_crime.sql
+++ shared_dbt/models/staging/los_angeles/stg_la_crime.sql
@@ -1,7 +1,3 @@
-{{ config (
-    materialized="table"
-)}}
-
 with stg_crime as (
     select * from {{ ref('stg_los_angeles') }}
 ),
@@ -9,10 +5,10 @@ with stg_crime as (
 
 final as (
     select distinct
-      crm_cd,
-      crm_cd_desc,
-      current_timestamp as created_at,
-      current_timestamp as modified_at
+        crm_cd,
+        crm_cd_desc,
+        current_timestamp as created_at,
+        current_timestamp as modified_at
     from stg_crime
 )
 
